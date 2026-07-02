97b32461d0f4d6a3f8069a87ae511dcde5edeead
edwardswanepoel2007 <edwardswanepoel7@gmail.com>
2026-07-02 19:48:24 +0200

    Add app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+// Rosslands Farm Manager
+// Version 0.1
+
+console.log("Rosslands Farm Manager loaded!");
+
+// Future data will go here
+const farm = {
+  camps: [],
+  animals: []
+};
+
+function startApp() {
+  console.log("Welcome to Rosslands!");
+}
+
+startApp();
